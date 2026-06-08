8cd0cb4c83a6dae712ecfb52f310c1aba91fe1ea
elnaznosrati <elnaznosrati39@gmail.com>
2026-06-08 20:31:00 +0330

    add Game Map class

---
+++ src/model/GameMap.java
@@ -0,0 +1,52 @@
+package model;
+
+import model.HexCoordinate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class GameMap {
+
+    private Map<HexCoordinate, Hex> hexes;
+
+    public Hex getHex(HexCoordinate coordinate) {
+        return hexes.get(coordinate);
+    }
+
+    public boolean containsCoordinate(HexCoordinate coordinate) {
+        return hexes.containsKey(coordinate);
+    }
+
+    public List<Hex> getNeighboursOf(HexCoordinate coordinate) {
+        List<Hex> neighbours = new ArrayList<>();
+
+        for (HexCoordinate neighborCoordinate : coordinate.findNeighbours()) {
+            Hex neighborHex = getHex(neighborCoordinate);
+
+            if (neighborHex != null) {
+                neighbours.add(neighborHex);
+            }
+        }
+
+        return neighbours;
+    }
+
+    public List<Hex> getHexesInRadius(HexCoordinate center, int radius) {
+        List<Hex> hexesInRadius = new ArrayList<>();
+        for (HexCoordinate hexCoordinate : hexes.keySet()) {
+            if (center.distanceTo(hexCoordinate) <= radius) {
+                hexesInRadius.add(hexes.get(hexCoordinate));
+            }
+        }
+        return hexesInRadius;
+    }
+
+    public List<Hex> getAllHexes() {
+        List<Hex> result = new ArrayList<>();
+        for (HexCoordinate hexCoordinate : hexes.keySet()) {
+            result.add(hexes.get(hexCoordinate));
+        }
+        return result;
+    }
+}
