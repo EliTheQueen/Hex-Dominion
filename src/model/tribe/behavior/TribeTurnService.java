f01b578bbc12f16f3a69d96312fedb3c9c6a8708
elnaznosrati <elnaznosrati39@gmail.com>
2026-08-05 15:07:36 +0330

    Add tribe turn behavior framework

---
+++ src/model/tribe/behavior/TribeTurnService.java
@@ -0,0 +1,28 @@
+package model.tribe.behavior;
+
+import model.tribe.Tribe;
+
+public class TribeTurnService {
+
+    private final TribeBehaviorStrategy behaviorStrategy;
+
+    public TribeTurnService(TribeBehaviorStrategy behaviorStrategy) {
+        if (behaviorStrategy == null) {
+            throw new IllegalArgumentException("behaviorStrategy must not be null");
+        }
+
+        this.behaviorStrategy = behaviorStrategy;
+    }
+
+    public TribeTurnAction processTurn(Tribe tribe, TribeTurnContext context) {
+        if (tribe == null || context == null) {
+            throw new IllegalArgumentException("arguments must not be null");
+        }
+
+        if (!tribe.isDiscovered() || tribe.isDefeated()) {
+            return TribeTurnAction.NONE;
+        }
+
+        return behaviorStrategy.chooseAction(tribe, context);
+    }
+}
